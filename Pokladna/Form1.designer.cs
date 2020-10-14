6428fea09bc7eac45f315ace1b98f3824e030587
Kamils6 <72434031+Kamils6@users.noreply.github.com>
2020-10-14 16:26:47 +0200

    Form1.designer.cs

---
+++ Pokladna/Form1.designer.cs
@@ -0,0 +1,123 @@
+namespace Pokladna_Dvoracek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.idDoklad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.datum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.castka = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.popis = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.zustatek = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.poznamka = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cislo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(569, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 666);
+            this.panel1.TabIndex = 0;
+            // 
+            // lvData
+            // 
+            this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idDoklad,
+            this.cislo,
+            this.datum,
+            this.popis,
+            this.castka,
+            this.zustatek,
+            this.poznamka});
+            this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvData.HideSelection = false;
+            this.lvData.Location = new System.Drawing.Point(0, 0);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(569, 666);
+            this.lvData.TabIndex = 0;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // idDoklad
+            // 
+            this.idDoklad.Text = "Č. dokladu";
+            // 
+            // datum
+            // 
+            this.datum.Text = "Datum";
+            // 
+            // castka
+            // 
+            this.castka.Text = "Částka";
+            // 
+            // popis
+            // 
+            this.popis.Text = "Popis";
+            // 
+            // zustatek
+            // 
+            this.zustatek.Text = "Zůstatek";
+            // 
+            // poznamka
+            // 
+            this.poznamka.Text = "Poznámka";
+            // 
+            // cislo
+            // 
+            this.cislo.Text = "Číslo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 666);
+            this.Controls.Add(this.lvData);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView lvData;
+        private System.Windows.Forms.ColumnHeader datum;
+        private System.Windows.Forms.ColumnHeader idDoklad;
+        private System.Windows.Forms.ColumnHeader popis;
+        private System.Windows.Forms.ColumnHeader castka;
+        private System.Windows.Forms.ColumnHeader zustatek;
+        private System.Windows.Forms.ColumnHeader poznamka;
+        private System.Windows.Forms.ColumnHeader cislo;
+    }
+}
